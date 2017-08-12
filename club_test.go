f319e59e5548933ef7f4573f9b36542497bccc2b
dc0d <kaveh.shahbazian@gmail.com>
2017-08-12 15:46:04 +0430

    clean up

--- club_test.go
+++ club_test.go
@@ -1,68 +1,18 @@
 package club
 
-// import (
-// 	"testing"
+import (
+	"testing"
+)
 
-// 	"github.com/stretchr/testify/assert"
-// )
-
-// func TestTimerScope(t *testing.T) {
-// 	// t.SkipNow()
-// 	defer TimerScope("")()
-// }
-
-// func TestHere(t *testing.T) {
-// 	funcName, fileName, fileLine, callerErr := Here()
-// 	assert.Equal(t, "TestHere", funcName)
-// 	assert.Equal(t, "club/club_test.go", fileName)
-// 	assert.Nil(t, callerErr)
-// 	assert.NotZero(t, fileLine)
-// }
-
-// // import (
-// // 	"testing"
-// // 	"time"
-
-// // 	"github.com/dc0d/goroutines"
-// // )
-
-// // func TestGroup(t *testing.T) {
-// // 	g := NewGroup()
-
-// // 	items := make(chan int, 2)
-// // 	g.Go(func() { items <- 1 })
-// // 	g.Go(func() { items <- 1 })
-// // 	g.Wait()
-// // 	close(items)
-// // 	acc := 0
-// // 	for v := range items {
-// // 		acc += v
-// // 	}
-// // 	if acc != 2 {
-// // 		t.Fail()
-// // 	}
-// // }
-
-// // func TestApp(t *testing.T) {
-// // 	err := goroutines.New().
-// // 		WaitStart().
-// // 		WaitGo(time.Second * 3).
-// // 		Go(func() {
-// // 			defer Finit(-1, time.Millisecond)
-// // 			AppPool.Go(func() { <-AppCtx.Done() })
-// // 			AppCancel()
-// // 			AppPool.Wait()
-// // 		})
-// // 	if err != nil {
-// // 		t.Fail()
-// // 	}
-// // }
-
-// // func TestErrors(t *testing.T) {
-// // 	var es Errors
-// // 	es = append(es, Error(`ONE`))
-// // 	es = append(es, Error(`TWO`))
-// // 	if es.String() != `[ONE] [TWO]` {
-// // 		t.Fail()
-// // 	}
-// // }
+func Test01(t *testing.T) {
+	defer TimerScope("")()
+	// // zap.NewProduction(zap.)
+	// logger, _ := zap.NewProduction()
+	// logger.Sugar()
+	// defer logger.Sync()
+	// logger.Info("failed to fetch URL",
+	// 	// Structured context as strongly typed Field values.
+	// 	zap.Int("attempt", 3),
+	// 	zap.Duration("backoff", time.Second),
+	// )
+}
