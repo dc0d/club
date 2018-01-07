06eec93b6c1e61426fb3d4030d29e6a2d425bce3
dc0d <kaveh.shahbazian@gmail.com>
2018-01-07 12:58:16 +0330

    log

---
+++ logger/log/logger.go
@@ -0,0 +1,16 @@
+package log
+
+import (
+	"github.com/dc0d/club/logger"
+)
+
+var (
+	_logger *logger.Logger
+)
+
+func init() {
+	_logger = logger.New()
+}
+
+// Logger .
+func Logger() *logger.Logger { return _logger }
